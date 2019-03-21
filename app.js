aefd865abc327939828f4f22ad7ddef1a64f0bc8
astrid <UO258194@uniovi.es>
2019-03-21 17:49:35 +0100

    “SDI-NodeJS-8.2 - Acceso a datos de Mongo desde Node.js”.
    Signed-off-by: astrid <UO258194@uniovi.es>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Módulos
+var mongo = require('mongodb');
 var express = require('express');
 var app = express();
 var swig = require('swig');
@@ -12,16 +13,15 @@ app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
+app.set('db','mongodb://admin:admin@cluster0-shard-00-00-4h3lp.mongodb.net:27017,cluster0-shard-00-01-4h3lp.mongodb.net:27017,cluster0-shard-00-02-4h3lp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
+
 
 //Rutas/controladores por lógica
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig);
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig,mongo);
 // lanzar el servid
 app.listen(app.get('port'), 8081, function() {
     console.log("Servidor activo");
 });
 
-mongodb://admin:admin@cluster0-shard-00-00-4h3lp.mongodb.net:27017,cluster0-shard-00-01-4h3lp.mongodb.net:27017,cluster0-shard-00-02-4h3lp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true

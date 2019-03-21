c114be16276b8decdd91a8164c7fb7a124529f7b
astrid <UO258194@uniovi.es>
2019-03-21 09:21:09 +0100

    “SDI-NodeJS- 6,7 División de las rutas en módulos, peticiones Get y parámetros”.
    Signed-off-by: astrid <UO258194@uniovi.es>

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ var app = express();
 // Variables
 app.set('port', 8081);
 
-app.get('/usuarios', function(req, res) {
-    console.log("Depurar aquó");
-    res.send('ver usuarios');
-});
-app.get('/canciones', function(req, res) {
-    res.send('ver canciones');
-});
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
 // lanzar el servid
 app.listen(app.get('port'), 8081, function() {
     console.log("Servidor activo");

62b8a00687b64ba1aeaddb17ab3a2b33ecfcb5de
astrid <UO258194@uniovi.es>
2019-03-21 10:17:24 +0100

    “SDI-NodeJS- 11,12,13,14 Vistas y motores de plantillas ”.
    Signed-off-by: astrid <UO258194@uniovi.es>

--- app.js
+++ app.js
@@ -23,3 +23,5 @@ require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
 app.listen(app.get('port'), 8081, function() {
     console.log("Servidor activo");
 });
+
+mongodb://admin:admin@cluster0-shard-00-00-4h3lp.mongodb.net:27017,cluster0-shard-00-01-4h3lp.mongodb.net:27017,cluster0-shard-00-02-4h3lp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true
